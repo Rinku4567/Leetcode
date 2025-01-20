548196ee058e04c9a77c6478f0dd050ced403c6f
Rinku Bathra <113289104+Rinku4567@users.noreply.github.com>
2025-01-20 09:01:58 +0530

    Update 003_Perfect_Number.java

--- 003_Perfect_Number.java
+++ 003_Perfect_Number.java
@@ -1,20 +1,16 @@
 class Solution {
-    public boolean checkPerfectNumber(int num) {
-        int sum=0;
-       for(int i=1;i<num;i++){
-            if(num%i==0){
+public boolean checkPerfectNumber(int num) {
+int sum=0;
+for(int i=1;i<num;i++){
+if(num%i==0){
 sum=sum+i;
-            }   
-       }
-       if(sum==num){
-        return true;
-       }
-       else{
-        return false;
-       }
-    }
-    public static void main(String[] args) {
-        Solution s1=new Solution();
-        s1.checkPerfectNumber(6);
+}   
+}
+if(sum==num){
+return true;
+}
+else{
+return false;
+} 
 }
 }
